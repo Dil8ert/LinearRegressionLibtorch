88f256d2092c2f819af8ce84a588091ac5cf29ec
Param Shelar <60221741+Dil8ert@users.noreply.github.com>
2021-10-14 14:37:11 +0530

    Add files via upload

---
+++ LinearRegressionPytorch.cpp
@@ -0,0 +1,50 @@
+﻿// LinearRegressionPytorch.cpp : Defines the entry point for the application.
+//
+
+#include "LinearRegressionPytorch.h"
+
+using namespace std;
+
+
+auto forward(torch::Tensor w,torch::Tensor x) {
+    return w * x;
+}
+
+auto loss(torch::Tensor y, torch::Tensor y_pred) {
+    auto sum = pow((y - y_pred), 2);
+    at::Tensor x = sum;
+    at::Tensor x_mean = x.mean();
+    torch::Tensor res = x_mean;
+    return res;
+}
+
+int main() {
+    torch::Tensor X = torch::tensor({ 1,2,3,4 });
+    torch::Tensor Y = torch::tensor({ 3,6,9,12 });
+    
+    auto w = torch::tensor(0.0,torch::requires_grad());
+    /*auto y_pred = forward(w, X);
+    
+    auto l = loss(Y, y_pred);
+    l.backward();
+    cout << w.grad() << endl;*/
+    
+    int i_ter = 100;
+    float learningRate = 0.01;
+   
+    for (int i = 0; i < i_ter; i++) {
+        auto y_pred = forward(w, X);
+        auto l = loss(Y, y_pred);
+        cout << i << " " << y_pred << " " << "Loss = " <<  l << endl;
+        l.backward();
+        torch::NoGradGuard guard;
+        w -= learningRate * w.grad();
+
+        w.mutable_grad() = torch::tensor(0.0);
+        
+        
+    }
+
+    
+    return 0;
+}
---
+++ LinearRegressionPytorch.h
@@ -0,0 +1,10 @@
+﻿// LinearRegressionPytorch.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <torch/torch.h>
+#include <cmath>
+#include <ATen/ATen.h>
+// TODO: Reference additional headers your program requires here.
